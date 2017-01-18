f1e5397482e3e80cc6879491246a445aaaa8b06a
Aaron Schachter <aaronschachter@gmail.com>
2017-01-18 15:36:25 -0800

    Adds comments

--- rotate.js
+++ rotate.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/**
+ * For each rotation, shift everything down an index.
+ */
 function rotate(array, d) {
   const length = array.length;
   let numRotations = 0;
@@ -14,5 +17,24 @@ function rotate(array, d) {
   console.log(`rotated:${array}`);
 }
  
+ /**
+  * We perform faster by just doing the shift once, but need extra buffer to store the values
+  * to rotate to the end of the array.
+  */
+ function rotateConstant(array, d) {
+  const rotateMe = [];
+  for (let i = 0; i < d; i++) {
+    rotateMe[i] = array[i];
+  }
+  for (let j = d; j < array.length; j++) {
+    array[j-d] = array[j];
+  }
+  for (let i = 0; i < d; i++) {
+    const index = array.length - d + i;
+    array[index] = rotateMe[i];
+  }
+  console.log(`rotated:${array}`);
+ }
+
  const array = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
- rotate(array, 4);
+ rotateConstant(array, 2);
